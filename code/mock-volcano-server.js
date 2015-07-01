dfd2969c41cb6f577d4a1fde15702ae19a8e693b
Korei Klein <korei.klein@gmail.com>
2015-07-01 10:53:38 -0400

    Write experimental OLP using a 'code environment' asset type for the mock server.

---
+++ code/mock-volcano-server.js
@@ -0,0 +1,20 @@
+
+// The mock volcano server.
+// It returns promises for
+var server {
+  getVolcano: function() {
+    // TODO(KK): Implement this.
+  },
+
+  getCurrentTime: function() {
+    // TODO(KK): Implement this.
+  },
+
+  getGeologists: function() {
+    // TODO(KK): Implement this.
+  },
+
+  getReadings: function(geologist) {
+    // TODO(KK): Implement this.
+  },
+};
